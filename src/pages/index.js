1ecb8153292ee87a582ef1820aa0e56c57452c76
Ben Mallinson <ben@benmallinson.me>
2019-04-26 10:31:58 +0100

    Updating for May

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ function IndexPage () {
         <div className='text-center flex-grow'>
           <div className='text-left md:pl-6'>
             <p className='text-3xl leading-tight text-blue font-bold'>NEXT EVENT:</p>
-            <p className='font-semibold text-lg text-purple'>THURSDAY 25TH APRIL 2019</p>
+            <p className='font-semibold text-lg text-purple'>TUESDAY 28TH MAY 2019</p>
           </div>
         </div>
       </div>
@@ -38,9 +38,12 @@ function IndexPage () {
         <div className='text-center flex-grow'>
           <div className='text-left md:pl-6'>
             <p className='text-3xl leading-tight text-blue font-bold'>DISCUSSING:</p>
-            <p className='font-semibold text-purple text-lg'>WHEN TO SAY ENOUGH</p>
-            <p className="text-purple italic text-md py-2">Taking a serious look at what empowers you to be a developer, but also looking at when you're no longer wanting to be a developer for where you are. </p>
-          </div>
+            <p className='font-semibold text-purple text-lg'>THE MAKING OF A GREAT API</p>
+            <p className="text-purple italic text-md py-2">Everyone is writing APIs but what makes a great one? Rob Allen will take you on a tour of the most important features that you should think about when creating an API.</p>
+            <div className="mb-2 mt-4">
+              <a className='py-3 px-8 bg-blue mx-auto rounded-lg w-32 overflow-hidden text-xl text-center font-bold leading-normal text-white hover:bg-purple no-underline' rel="noopener noreferrer" href='https://www.meetup.com/StackChat/events/259589149/' target='_blank'>RSVP</a>
+            </div>
+            </div>
         </div>
       </div>
       <div className='flex items-center px-6 py-4 bg-white p-4 mx-auto w-full rounded-lg overflow-hidden mt-6'>
@@ -53,7 +56,7 @@ function IndexPage () {
           </div>
         </div>
       </div>
-      <a className='mt-6 p-3 bg-white mx-auto rounded-lg w-full overflow-hidden text-xl text-center font-bold rounded-lg leading-normal bg-white text-purple hover:bg-blue hover:text-white no-underline' rel="noopener noreferrer" href='https://www.meetup.com/StackChat/' target='_blank'>FIND OUT MORE</a>
+      <a className='mt-6 p-3 bg-blue mx-auto rounded-lg w-full overflow-hidden text-xl text-center font-bold rounded-lg leading-normal text-white hover:bg-purple no-underline' rel="noopener noreferrer" href='https://www.meetup.com/StackChat/' target='_blank'>FIND OUT MORE</a>
       <div className='flex items-center justify-center text-center mt-4 py-4 mx-auto w-full rounded-lg overflow-hidden'>
         <div className='text-left'>
           <a href='https://www.twitter.com/stackchatuk' rel="noopener noreferrer"  target='_blank'><img src={twitter} alt="twitter logo" className='social-img max-w-sm max-h-sm h-16 m-2' /></a>
